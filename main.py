77c8c803ed8ada06e4601768eef9d09d09c45a05
Gopal Jajam <61733665+Gopaljajam@users.noreply.github.com>
2023-03-26 08:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 import os
-print(os.getcwd())
+from tkinter import Tk, filedialog
 
 # Prompt user to input video URL
 video_url = input("Enter the YouTube video URL: ")
@@ -20,25 +20,26 @@ while True:
 # Create a YouTube object from the URL
 yt = YouTube(video_url)
 
+# Prompt user to choose a directory to save the file to
+root = Tk()
+root.withdraw()
+save_dir = filedialog.askdirectory()
+
 if download_video:
     # Get the highest resolution video stream
     stream = yt.streams.get_highest_resolution()
-    # Download the video to the current directory
+    # Download the video to the specified directory
     print(f"Downloading video '{yt.title}'...")
-    stream.download()
+    stream.download(output_path=save_dir)
     print("Download complete!")
 else:
     # Get the highest quality audio stream
     stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4").first()
-    # Download the audio to the current directory as an mp4 file
+    # Download the audio to the specified directory as an mp4 file
     print(f"Downloading audio from '{yt.title}'...")
-    audio_file = stream.download()
+    audio_file = stream.download(output_path=save_dir)
     # Rename the audio file to have an mp3 extension
     base, ext = os.path.splitext(audio_file)
     new_file = f"{base}.mp3"
     os.rename(audio_file, new_file)
     print("Download complete!")
-
-
-
-
